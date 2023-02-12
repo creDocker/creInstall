a858eac235c9820f4bf180fa4acb9f11f143b4b7
Michael Kahle <michael.kahle@yahoo.de>
2023-02-12 17:34:54 +0100

    check sdk, exit 1

--- cre/node-test.sh
+++ cre/node-test.sh
@@ -25,7 +25,7 @@ else
    echo "[FAIL]: no node version found!"
 fi
 rightVersion=$(echo $isInFile | sed -e "s|.*node.* v${NODE_VERSION}\..*\..*|v${NODE_VERSION}.y.z|")
-if [[ "$rightVersion" = "v${NODE_VERSION}.2.3" ]]; then
+if [[ "$rightVersion" = "v${NODE_VERSION}.y.z" ]]; then
    echo "[SUCCESS]: right node version found!"
 else
    echo "[FAIL]: wrong node version found! ($rightVersion)vs(${NODE_VERSION})"
@@ -45,5 +45,20 @@ else
    echo "[FAIL]: no npm version found!"
 fi
 
+## check sdk
+isInFile=$(cat /cre/versions.txt | grep "sdk")
+if [ -z "$isInFile" ]; then
+    echo "[FAIL]: sdk not installed!"
+    #exit 1
+fi
+
+anyVersion=$(echo $isInFile | sed -e 's|.*sdk.* .*\..*\..*|1.2.3|')
+if [[ "$anyVersion" = "1.2.3" ]]; then
+   echo "[SUCCESS]: some sdk version found!"
+else
+   echo "[FAIL]: no sdk version found!"
+fi
+
+exit 1
 echo "[SUCCESS]"
 exit 0

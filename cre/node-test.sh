9a2bafa058d881581cc17cf2c837c733f4008d31
Michael Kahle <michael.kahle@yahoo.de>
2023-02-12 17:06:17 +0100

    check versions

--- cre/node-test.sh
+++ cre/node-test.sh
@@ -14,18 +14,18 @@ cat /cre/versions.txt
 
 ## check NODE_VERSION
 isInFile=$(cat /cre/versions.txt | grep "node")
-if [ -z $isInFile ]; then
+if [ -z "$isInFile" ]; then
     echo "[FAIL]: node not installed!"
     #exit 1
 fi
 anyVersion=$(echo $isInFile | sed -e 's|.*node.* v.*\..*\..*|v1.2.3|')
-if [[ $anyVersion = v1.2.3 ]]; then
+if [[ "$anyVersion" = "v1.2.3" ]]; then
    echo "[SUCCESS]: some node version found!"
 else
    echo "[FAIL]: no node version found!"
 fi
 rightVersion=$(echo $isInFile | sed -e 's|.*node.* v${NODE_VERSION}\..*\..*|vXX.2.3|')
-if [[ $anyVersion = vXX.2.3 ]]; then
+if [[ "$anyVersion" = "vXX.2.3" ]]; then
    echo "[SUCCESS]: right node version found!"
 else
    echo "[FAIL]: wrong node version found!"
@@ -33,13 +33,13 @@ fi
 
 ## check npm
 isInFile=$(cat /cre/versions.txt | grep "npm")
-if [ -z $isInFile ]; then
+if [ -z "$isInFile" ]; then
     echo "[FAIL]: npm not installed!"
     #exit 1
 fi
 
 anyVersion=$(echo $isInFile | sed -e 's|.*sdk.* .*\..*\..*|1.2.3|')
-if [[ $anyVersion = 1.2.3 ]]; then
+if [[ "$anyVersion" = "1.2.3" ]]; then
    echo "[SUCCESS]: some npm version found!"
 else
    echo "[FAIL]: no npm version found!"

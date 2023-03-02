29244088d597d3191423315e6d1a73e9c8d7c76e
Michael Kahle <michael.kahle@yahoo.de>
2023-03-02 22:01:22 +0100

    add script to install java

--- cre/sdk-java.sh
+++ cre/sdk-java.sh
@@ -5,10 +5,17 @@
 ## source ${SDKMAN_DIR}/bin/sdkman-init.sh
 ## sdk $@
 
+JAVA_FAMILY="open"
+# JAVA_FAMILY="tem"
+# JAVA_FAMILY="mnspecial"
+# JAVA_FAMILY="$@"
+
 /cre/sdk.sh list java
 
-SDK_JAVA="$(/cre/sdk.sh list java | grep ' 11\..*-open' | head -n 1 | cut --delimiter=\| --fields=6 |sed 's/ *//g')"
+SDK_JAVA="$(/cre/sdk.sh list java | grep ' $JAVA_VERSION\..*-$JAVA_FAMILY' | head -n 1 | cut --delimiter=\| --fields=6 |sed 's/ *//g')"
 
 echo "java-base: $JAVA_VERSION"
 echo "java-sdk : $SDK_JAVA"
+
+#/cre/sdk.sh install java $SDK_JAVA
 

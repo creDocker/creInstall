d744f00063bb6742bee489142fe5a970a3c870c4
Michael Kahle <michael.kahle@yahoo.de>
2023-03-03 11:39:49 +0100

    handling of java version and family

--- cre/sdk-java.sh
+++ cre/sdk-java.sh
@@ -5,37 +5,37 @@
 ## source ${SDKMAN_DIR}/bin/sdkman-init.sh
 ## sdk $@
 
-JAVA_FAMILY=open
+# JAVA_FAMILY=open
 # JAVA_FAMILY="tem"
 # JAVA_FAMILY="grl"   ## GraalVM  >=19
 # JAVA_FAMILY="$@"
+sdkfamily="$1"
+if [ $sdkfamily -eq 0 ]; then
+ sdkfamily=$JAVA_FAMILY
+fi
+sdkversion="$2"
+if [ $sdkversion -eq 0 ]; then
+ sdkversion=$JAVA_VERSION
+fi
 
-/cre/sdk.sh list java
+echo "possible families for version ${sdkversion}:"
+/cre/sdk.sh list java | grep " ${sdkversion}\..*-.*" | cut --delimiter=\| --fields=4 | sed 's/ *//g' | sort -u | sed -z 's/\n/, /g'
 
-echo "possible families for version ${JAVA_VERSION}:"
-/cre/sdk.sh list java | grep " ${JAVA_VERSION}\..*-.*" | cut --delimiter=\| --fields=4 | sed 's/ *//g' | sort -u
+echo "possible versions for family ${$sdkfamily}:"
+/cre/sdk.sh list java | grep " .*\..*-${$sdkfamily}" | cut --delimiter=\| --fields=3 | sed 's/ *//g' | sed 's/\..*//g' | sort -u -n | sed -z 's/\n/, /g'
 
-echo "possible versions for family ${JAVA_FAMILY}:"
-/cre/sdk.sh list java | grep " .*\..*-${JAVA_FAMILY}" | cut --delimiter=\| --fields=3 | sed 's/ *//g' | sed 's/\..*//g' | sort -u -n
-
-##SDK_JAVA="$(/cre/sdk.sh list java | grep ' ${JAVA_VERSION}\..*-${JAVA_FAMILY}' | head -n 1 | cut --delimiter=\| --fields=6 | sed 's/ *//g' 2>&1)"
-#SDK_JAVA="$(/cre/sdk.sh list java | grep ' 11\..*-open' | head -n 1 | cut --delimiter=\| --fields=6 |sed 's/ *//g' 2>&1)"
-##SDK_JAVA="$(/cre/sdk.sh list java | grep ' ${JAVA_VERSION}\..*-open' | head -n 1 | cut --delimiter=\| --fields=6 |sed 's/ *//g' 2>&1)"
-SDK_JAVA="$(/cre/sdk.sh list java | grep ' 11\..*-${JAVA_FAMILY}' | head -n 1 | cut --delimiter=\| --fields=6 |sed 's/ *//g' 2>&1)"
-##SDK_JAVA="$(/cre/sdk.sh list java | grep \" ${JAVA_VERSION}\..*-${JAVA_FAMILY}\" | head -n 1 | cut --delimiter=\| --fields=6 | sed 's/ *//g' 2>&1)"
-
-
-
-COMMAND=" /cre/sdk.sh list java | grep ' ${JAVA_VERSION}\..*-${JAVA_FAMILY}' | head -n 1 | cut --delimiter=\| --fields=6 | sed 's/ *//g' "
-echo $COMMAND
-#SDK_JAVA="$($COMMAND)"
-SDK_JAVA=$(eval "$COMMAND")
-
+cmd=" /cre/sdk.sh list java | grep ' ${sdkversion}\..*-${$sdkfamily}' | head -n 1 | cut --delimiter=\| --fields=6 | sed 's/ *//g' "
+SDK_JAVA=$(eval "$cmd")
 
 echo "java-base: $JAVA_VERSION"
 echo "java-sdk : $SDK_JAVA"
 
-
 ## TODO check if $SDK_JAVA is set 
-## /cre/sdk.sh install java $SDK_JAVA
+if [ $SDK_JAVA -eq 0 ]; then
+   echo "[FAIL]: no corresponding java version (${sdkfamily}, #${sdkversion}) found!"
+   /cre/sdk.sh list java
+else
+   /cre/sdk.sh install java $SDK_JAVA
+fi
+## 
 
